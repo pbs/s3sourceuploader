93d43839e8bce2979c9cc1ec1460113f9d6dae66
Sever Băneşiu <banesiu.sever@gmail.com>
2012-06-29 09:45:38 +0300

    Bump version.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='s3sourceuploader',
-    version='0.1.1',
+    version='0.1.2',
     description=('Add an upload to s3 command to setuptools.'),
     keywords='setuptools s3 upload',
     author='Sever Banesiu',
@@ -10,8 +10,8 @@ setup(
     url='http://github.com/pbs/s3sourceuploader',
     license='BSD',
     py_modules=['s3sourceuploader'],
-    install_requires = [ "boto", ],
-    entry_points = {
-        "distutils.commands": [ "s3upload = s3sourceuploader:UploadSource" ]
+    install_requires=["boto"],
+    entry_points={
+        "distutils.commands": ["s3upload = s3sourceuploader:UploadSource"]
     }
 )
